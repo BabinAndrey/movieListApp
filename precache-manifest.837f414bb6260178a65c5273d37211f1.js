b499b2f62dd3a38cc16dc3fc819c5a6b51d4c030
BabinAndrey <babinandre@mail.ru>
2020-04-12 11:46:12 +0300

    Updates

---
+++ precache-manifest.837f414bb6260178a65c5273d37211f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fa7a242790b03afff56ef9754754073",
+    "url": "/movie-list-app/index.html"
+  },
+  {
+    "revision": "0be23b33ee018b6ccc04",
+    "url": "/movie-list-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ba7a2d4248eef6b5ba32",
+    "url": "/movie-list-app/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "0be23b33ee018b6ccc04",
+    "url": "/movie-list-app/static/js/2.44345ff3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/movie-list-app/static/js/2.44345ff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba7a2d4248eef6b5ba32",
+    "url": "/movie-list-app/static/js/main.4f5dbb27.chunk.js"
+  },
+  {
+    "revision": "c719ff171c56b70ed1b4",
+    "url": "/movie-list-app/static/js/runtime-main.0e4a974c.js"
+  }
+]);
